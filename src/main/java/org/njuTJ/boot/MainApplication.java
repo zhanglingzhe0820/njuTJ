a980b4fa6390a5ee141638ce413f61337a734ff8
HUGH <445073309@qq.com>
2017-11-30 01:08:01 +0800

    add gradle depandency and init spring boot

---
+++ src/main/java/org/njuTJ/boot/MainApplication.java
@@ -0,0 +1,13 @@
+package org.njuTJ.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(MainApplication.class, args);
+    }
+}
