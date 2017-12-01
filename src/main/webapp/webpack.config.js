2858accdad54c14eb68ca964c2f24f46c0b20970
HUGH <445073309@qq.com>
2017-12-01 14:19:53 +0800

    init web content framework

---
+++ src/main/webapp/webpack.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    devtool: 'eval-source-map',
+    entry:  __dirname + "/app/main.js",//入口文件
+    output: {
+      path: __dirname + "/public",//打包后的文件存放的地方
+      filename: "bundle.js"//打包后输出文件的文件名
+    },
+    devServer: {
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        inline: true//实时刷新
+      },
+      module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+  }
